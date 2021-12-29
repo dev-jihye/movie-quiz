73f298ab34f0fba236ec927c6ca3e4556a15e634
Sophie <jihye340@gmail.com>
2021-12-29 23:53:30 +0900

    Revert "Login state"
    This reverts commit dce53de45a68b9b835d72b91d4a95531c914c68c.

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,19 @@
-import { ApolloProvider, useReactiveVar } from "@apollo/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { client, isAuthVar } from "./apollo";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ROUTE } from "./constance";
 import CreateQuiz from "./screens/CreateQuiz";
-import CreateUser from "./screens/CreateUser";
-import Error404 from "./screens/Error404";
 import Home from "./screens/Home";
-import Login from "./screens/Login";
 import MyProfile from "./screens/MyProfile";
 import ShowQuiz from "./screens/ShowQuiz";
 
 export default function App() {
-  const isAuth = useReactiveVar(isAuthVar);
-
   return (
-    <ApolloProvider client={client}>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path={ROUTE.LOGIN}
-            element={isAuth ? <Navigate to={ROUTE.HOME} /> : <Login />}
-          />
-          <Route
-            path={ROUTE.CREATE_USER}
-            element={isAuth ? <Navigate to={ROUTE.HOME} /> : <CreateUser />}
-          />
-          <Route path={ROUTE.HOME} element={<Home />} />
-          <Route path={`${ROUTE.QUIZ}/:id`} element={<ShowQuiz />} />
-          <Route path={ROUTE.CREATE_QUIZ} element={<CreateQuiz />} />
-          <Route path={`${ROUTE.MY_PROFILE}/:id`} element={<MyProfile />} />
-          <Route path={"*"} element={<Error404 />} />
-        </Routes>
-      </BrowserRouter>
-    </ApolloProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTE.HOME} element={<Home />} />
+        <Route path={`${ROUTE.QUIZ}/:id`} element={<ShowQuiz />} />
+        <Route path={ROUTE.CREATE_QUIZ} element={<CreateQuiz />} />
+        <Route path={`${ROUTE.MY_PROFILE}/:id`} element={<MyProfile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/components/auth/FormError.tsx
+++
@@ -1,7 +0,0 @@
-interface IformError {
-  message: string | null;
-}
-
-export default function FormError({ message }: IformError) {
-  return message ? <div className="text-sm text-red-700">{message}</div> : null;
-}
